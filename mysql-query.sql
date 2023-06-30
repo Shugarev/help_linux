007577a5db37d55cb2384c10a23508ca56302f68
shugarev <sergey.s@tyrellcorp.work>
2023-06-30 12:23:40 +0300

    Изменения пользователя для выкладывания релиз ibs

--- mysql-query.sql
+++ mysql-query.sql
@@ -27,3 +27,25 @@ UPDATE operation SET created= DATE_SUB(created, INTERVAL 2 DAY) WHERE id > 70;
 
 SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_NAME`='summary_stat' AND `COLUMN_NAME` LIKE '%cashflow%';
 SELECT operation_type,operation_status,operation_amount,cashflow_currency,order_card_type,merchant_id,project_id,terminal_id,bank_id, day FROM summary_stat;
+
+
+mysqld --innodb-buffer-pool-size=9G --innodb-buffer-pool-instances=16
+SELECT @@innodb_buffer_pool_size/1024/1024/1024;
+
+
+mysql -uroot -h127.0.0.1 -P3306 -pesxErUHv0fotUN+ixTYHoDQix*U
+SET global general_log = 1;
+SET global log_output = 'table';
+
+-- clean log table
+truncate mysql.general_log;
+select * from mysql.general_log;
+
+
+-- set profiling https://dev.mysql.com/doc/refman/8.0/en/show-profile.html
+mysql -ubox -h127.0.0.1 -P3306 -paZ%34sdfDFtr test_base
+SET profiling = 1;
+SELECT @@profiling;
+
+source  /home/sergey/data/summary-report-slowly/request_w_opt.txt
+SHOW PROFILES;
